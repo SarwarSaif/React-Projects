1001bacb0964d9455875d1fd90484191790fb0b1
SarwarSaif <saifmu6@gmail.com>
2019-12-25 01:31:32 +0600

    Updates

---
+++ precache-manifest.e193540c433005c413e744301243fdf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36faca73c9b580165b825291347c5158",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "e35030c6773d396ff732",
+    "url": "/Monster-Rolodex/static/css/main.50cf9715.chunk.css"
+  },
+  {
+    "revision": "bfc9270e8b0016f6ed2b",
+    "url": "/Monster-Rolodex/static/js/2.421ed30e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monster-Rolodex/static/js/2.421ed30e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e35030c6773d396ff732",
+    "url": "/Monster-Rolodex/static/js/main.f67761bb.chunk.js"
+  },
+  {
+    "revision": "05de4a9118e484b28580",
+    "url": "/Monster-Rolodex/static/js/runtime-main.4518009a.js"
+  }
+]);
