9fd9abd484b74e6673f1585a716b31557df316cf
SarwarSaif <saifmu6@gmail.com>
2019-12-31 20:59:56 +0600

    Added Custom FormInput and Custom Button Component

---
+++ dalal-clothing/src/firebase/firebase.utils.js
@@ -0,0 +1,33 @@
+import firebase from 'firebase/app'; // Must import it
+import 'firebase/firestore'; // For the database
+import 'firebase/auth'; // For the authentication
+
+const config = {
+    apiKey: "AIzaSyA3VG47773pq1vRZSNf4QIWC2WorvDLonQ",
+    authDomain: "dalal-clothing-db.firebaseapp.com",
+    databaseURL: "https://dalal-clothing-db.firebaseio.com",
+    projectId: "dalal-clothing-db",
+    storageBucket: "dalal-clothing-db.appspot.com",
+    messagingSenderId: "214977339891",
+    appId: "1:214977339891:web:4ad2c66884db04e15bb286",
+    measurementId: "G-2CPXK926F9"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+// Setting Up google authentication 
+
+
+
+
+
+
+
+
+
+
+
+
